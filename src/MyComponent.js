fe1b7da7ee5b3e828323061da4059db16ce56525
Jose616dop <jose9596979899@gmail.com>
2021-06-25 19:21:55 -0500

    second commit

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -30,7 +30,7 @@ export class MyComponent extends LitElement {
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
+      <h2>José Sandoval: ${this.title} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
     `;
   }
